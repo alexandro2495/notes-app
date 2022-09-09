cd7cc55cc0b53a534461e285bb94ac713520d3c5
alexandro2495 <agarciar95@gmail.com>
2022-09-09 13:33:21 -0400

    Added project for Unit test with Nunit, Ninject and Moq

---
+++ Notes.UTests/BaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Ninject;
+using NUnit.Framework;
+
+namespace Notes.UTests
+{
+    public class BaseTests
+    {
+        protected IKernel Kernel { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            //prepare mock and interfaces to be used
+            //for scenarios later
+            //Kernel is from Niject is our guy
+            //to specify the dependencies and their
+            //concrete classes by using modules or manual
+            //references
+            Kernel = new StandardKernel();
+            Kernel.Load(Assembly.GetExecutingAssembly());
+        }
+    }
+}
