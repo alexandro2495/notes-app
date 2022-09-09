26ca246dfa2da684b09204806fc24389d1954a55
Carlos Lopez <carlosa.lopez.2022@outlook.com>
2022-09-09 17:48:17 -0400

    Added collectionview, notetemplateselector, and custom markup extension and images

---
+++ Notes/Notes/Pages/NotesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Notes.Pages
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
