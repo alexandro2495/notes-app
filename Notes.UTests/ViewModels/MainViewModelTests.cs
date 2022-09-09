32a6e36acf70f7cad8f4fa0c5dbfa876d90e8d11
alexandro2495 <35077192+alexandro2495@users.noreply.github.com>
2022-09-09 15:27:54 -0400

    Added project for Unit test with Nunit, Ninject and Moq (#4)

---
+++ Notes.UTests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Ninject;
+using Notes.ViewModels;
+using NUnit.Framework;
+
+namespace Notes.UTests.ViewModels
+{
+    public class MainViewModelTests : BaseTests
+    {
+        public MainViewModelTests()
+        {
+        }
+
+        [Test]
+        public void GetNotes_Have_To_Return_Notes()
+        {
+            // 1 - Arrange
+            var mainViewModel = Kernel.Get<MainViewModel>();
+
+            // 2 - Act
+            mainViewModel.GetNotesByUser();
+
+            // 3 - Assert
+            Assert.IsNotNull(mainViewModel.Notes);
+            Assert.IsNotEmpty(mainViewModel.Notes);
+            Assert.GreaterOrEqual(mainViewModel.Notes.Count, 1);
+        }
+    }
+}
+
--- Notes/Notes/ViewModels/MainViewModel.cs
+++ Notes/Notes/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Notes.Data.Models;
 using Notes.Services;
 using Prism.Mvvm;
 using Prism.Navigation;
@@ -8,6 +10,7 @@ namespace Notes.ViewModels
     public class MainViewModel : BindableBase,  INavigationAware
     {
         private readonly INavigationService _navigationService;
+        private readonly INoteService _noteService;
 
         private string _title;
         public string Title
@@ -16,10 +19,24 @@ namespace Notes.ViewModels
             set => SetProperty(ref _title, value);
         }
 
+        private ObservableCollection<Note> _notes;
+        public ObservableCollection<Note> Notes
+        {
+            get => _notes;
+            set => SetProperty(ref _notes, value);
+        }
+
         public MainViewModel(INavigationService navigationService, INoteService noteService, IUserService userService)
         {
             _navigationService = navigationService;
+            _noteService = noteService;
             Title = "Hey there is a binding over here!!";
+
+            Notes = new ObservableCollection<Note>();
+        }
+
+        public void GetNotesByUser() {
+            Notes = new ObservableCollection<Note>(_noteService.GetNotes(1));
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters)
