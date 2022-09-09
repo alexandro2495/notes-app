8e56e92c572e9d5255b0ad0bff327009090f98be
Carlos Lopez <carlosa.lopez.2022@outlook.com>
2022-09-09 00:03:10 -0400

    added logout page

---
+++ Notes/Notes/Pages/LogOutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Notes.Pages
+{
+    public partial class LogOutPage : ContentPage
+    {
+        public LogOutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
